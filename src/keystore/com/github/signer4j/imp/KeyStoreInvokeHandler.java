dcb2242abf4473a6fb9d736aebd4e47b4d5cd68c
Leonardo de Lima Oliveira <l3onardo.oliv3ira@gmail.com>
2021-02-27 18:34:54 -0400

    Fix access modifier

--- src/keystore/com/github/signer4j/imp/KeyStoreInvokeHandler.java
+++ src/keystore/com/github/signer4j/imp/KeyStoreInvokeHandler.java
@@ -20,9 +20,9 @@ import com.github.signer4j.imp.exception.OutOfMemoryException;
 import com.github.signer4j.imp.exception.PrivateKeyNotFound;
 import com.github.signer4j.imp.exception.TokenLockedException;
 
-class KeyStoreInvokeHandler extends InvokeHandler<KeyStoreAccessException> {
+public class KeyStoreInvokeHandler extends InvokeHandler<KeyStoreAccessException> {
   
-  static final KeyStoreInvokeHandler INVOKER = new KeyStoreInvokeHandler();
+  public static final KeyStoreInvokeHandler INVOKER = new KeyStoreInvokeHandler();
   
   private KeyStoreInvokeHandler() {}
   
