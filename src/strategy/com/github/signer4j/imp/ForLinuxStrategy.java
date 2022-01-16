3c3fa00ecfd6561cbcbe8b48173593aa46e81fd9
Leonardo de Lima Oliveira <l3onardo-oliv3ira@gmail.com>
2022-01-15 22:19:41 -0400

    improve lib dettection

--- src/strategy/com/github/signer4j/imp/ForLinuxStrategy.java
+++ src/strategy/com/github/signer4j/imp/ForLinuxStrategy.java
@@ -16,11 +16,9 @@ class ForLinuxStrategy extends PreloadedStrategy {
     load("/usr/lib/libeToken.so.4");
     load("/usr/lib/libcmP11.so");
     load("/usr/lib/libwdpkcs.so");
-    load("/usr/local/lib64/libwdpkcs.so");
     load("/usr/local/lib/libwdpkcs.so");
     load("/usr/lib/watchdata/ICP/lib/libwdpkcs_icp.so");
     load("/usr/lib/watchdata/lib/libwdpkcs.so");
-    load("/opt/watchdata/lib64/libwdpkcs.so");
     load("/usr/lib/libaetpkss.so.3");
     load("/usr/lib/libaetpkss.so.3.0");
     load("/usr/lib/opensc-pkcs11.so");
@@ -28,13 +26,17 @@ class ForLinuxStrategy extends PreloadedStrategy {
     load("/usr/local/ngsrv/libepsng_p11.so.1.2.2");
     load("/usr/local/AWP/lib/libOcsCryptoki.so");
     load("/usr/lib/libscmccid.so");
-    load("/usr/lib64/libeToken.so");
     load("/opt/ePass2003-Castle-20141128/i386/redist/libcastle.so.1.0.0");
+    load("/usr/lib/libneoidp11.so");
+    load("/usr/lib/opensc/opensc­pkcs11.so");
+    //64bits
     load("/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so");
     load("/usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so");
-    load("/usr/lib/libneoidp11.so");
     load("/usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so");
-    load("/usr/lib/opensc/opensc­pkcs11.so");
+    load("/opt/Assistente Desktop birdID/resources/extraResources/linux/x64/vault-pkcs11.so");
+    load("/usr/lib64/libeToken.so");
+    load("/usr/local/lib64/libwdpkcs.so");
+    load("/opt/watchdata/lib64/libwdpkcs.so");
   }
 }
 
--- src/strategy/com/github/signer4j/imp/ForMacStrategy.java
+++ src/strategy/com/github/signer4j/imp/ForMacStrategy.java
@@ -3,6 +3,7 @@ package com.github.signer4j.imp;
 class ForMacStrategy extends PreloadedStrategy {
 
   //TODO revisar libraries 64bits
+  
   public ForMacStrategy() {
     load("/usr/lib/libwdpkcs.dylib");
     load("/usr/local/lib/libwdpkcs.dylib"); 
@@ -10,9 +11,12 @@ class ForMacStrategy extends PreloadedStrategy {
     load("/usr/local/lib/libetpkcs11.dylib");
     load("/usr/local/lib/libaetpkss.dylib");
     load("/usr/local/lib/libbanrisulpkcs11.so");
+    load("/usr/local/lib/libdesktopID_Provider.dylib");
     load("/Applications/NeoID Desktop.app/Contents/Java/tools/macos/libneoidp11.dylib");
     load("/Applications/WatchKey USB Token Admin Tool.app/Contents/MacOS/lib/libWDP11_BR_GOV.dylib");
     load("/Applications/tokenadmin.app/Contents/Frameworks/libaetpkss.dylib");
     load("/Library/Frameworks/eToken.framework/Versions/A/libeToken.dylib");
+    //64bits
+    load("/Applications/Assistente Desktop birdID.app/Contents/resources/extraResources/osx/x64/vault-pkcs11.dylib");
   }
 }
